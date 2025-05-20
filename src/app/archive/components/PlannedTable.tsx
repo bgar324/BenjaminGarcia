63f20b80226dac8d2c3cca904f94c6e8922267c2
bgar324 <bentgarcia05@gmail.com>
2025-05-19 23:27:31 -0700

    oops

--- src/app/archive/components/PlannedTable.tsx
+++ src/app/archive/components/PlannedTable.tsx
@@ -114,7 +114,7 @@ export default function ArchiveTable() {
                   >
                     {title}
                   </a>
-                  {status === "Building" && (
+                  {status === "In Progress" && (
                     <span className="relative flex h-2 w-2">
                       <span className="animate-slow-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                       <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
