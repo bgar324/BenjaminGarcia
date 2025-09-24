960e22ab6bc55b46a7984974f05577498fb23842
bgar324 <bentgarcia05@gmail.com>
2025-09-23 22:05:11 -0700

    added project titles

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -48,21 +48,21 @@ export default function ArchivePage() {
 
       <section className="mt-10">
         <h2 className="w-fit border border-gray-300 dark:border-gray-700 rounded-md px-2 py-1 uppercase mt-10 tracking-wider lg:py-[.5px] flex items-center gap-1 text-lg md:text-xl font-bold mb-5">
-          Planned Projects
+          Planned Projects (3)
         </h2>
         <PlannedTable />
       </section>
 
       <section>
         <h2 className="w-fit border border-gray-300 dark:border-gray-700 rounded-md px-2 py-1 uppercase mt-10 tracking-wider lg:py-[.5px] flex items-center gap-1 text-lg md:text-xl font-bold mb-5">
-          Past Projects
+          Past Projects (6)
         </h2>
         <ArchiveTable />
       </section>
 
       <section>
         <h2 className="w-fit border border-gray-300 dark:border-gray-700 rounded-md px-2 py-1 uppercase mt-10 tracking-wider lg:py-[.5px] flex items-center gap-1 text-lg md:text-xl font-bold mb-5">
-          Scrapped Projects
+          Scrapped Projects (3)
         </h2>
         <ScrappedTable />
       </section>
