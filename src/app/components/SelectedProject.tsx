a1ab2ea97d548e40ddf20285b688bdc99242b4c6
bgar324 <bentgarcia05@gmail.com>
2026-03-16 16:59:17 -0700

    better icon placemenet

--- src/app/components/SelectedProject.tsx
+++ src/app/components/SelectedProject.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { ReactNode } from "react";
 import Image from "next/image";
+import { GitHubMarkIcon } from "../svgs/Icons";
 
 interface ProjectItemProps {
   src: string;
@@ -35,7 +36,7 @@ export default function ProjectItem({
     if (onImageClick) {
       onImageClick(
         galleryImages && galleryImages.length > 0 ? galleryImages : [src],
-        0,
+        0
       );
     }
   };
@@ -43,20 +44,20 @@ export default function ProjectItem({
   return (
     <div
       className="
+      relative
       flex flex-col md:flex-row gap-4 group rounded-xl border border-gray-200 dark:border-gray-600 
       p-3 transition-all duration-300 
       bg-white dark:bg-black md:items-start shadow-xs
     "
     >
-      
       <div
         className="
-    w-full md:w-1/3   
-    relative 
-    aspect-video md:aspect-auto 
-    md:h-[130px]
-    flex-shrink-0
-  "
+        w-full md:w-1/3   
+        relative 
+        aspect-video md:aspect-auto 
+        md:h-[130px]
+        flex-shrink-0
+      "
       >
         <Image
           src={src}
@@ -65,10 +66,10 @@ export default function ProjectItem({
           quality={82}
           sizes="(max-width: 768px) 100vw, 33vw"
           className="
-      rounded-lg
-      border border-gray-300 dark:border-gray-700
-      object-cover
-    "
+          rounded-lg
+          border border-gray-300 dark:border-gray-700
+          object-cover
+        "
           onClick={(e) => {
             e.preventDefault();
             handleImageClick();
@@ -78,11 +79,17 @@ export default function ProjectItem({
 
       <div className="flex flex-col justify-between md:w-2/3">
         <div className="flex flex-col">
-          <div className="flex items-center gap-3">
+          <div className="flex items-center justify-between gap-3">
             <h2 className="text-gray-900 dark:text-slate-100 font-semibold text-lg md:text-xl">
               {title}
             </h2>
-            <div className="flex items-center gap-4 text-center pb-[1px]">
+
+            <div
+              className="
+              flex items-center gap-4
+              md:absolute md:top-3 md:right-3
+            "
+            >
               {link && (
                 <a
                   href={link}
@@ -108,6 +115,7 @@ export default function ProjectItem({
                   </svg>
                 </a>
               )}
+
               {githubLink && (
                 <a
                   href={githubLink}
@@ -116,20 +124,7 @@ export default function ProjectItem({
                   className="text-gray-600 dark:text-slate-400 hover:text-gray-900 dark:hover:text-slate-200 transition-colors duration-200 flex items-center focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 focus-visible:rounded-sm"
                   title="View on GitHub"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                    <path d="M9 18c-4.51 2-5-2-7-2" />
-                  </svg>
+                  <GitHubMarkIcon />
                 </a>
               )}
             </div>
@@ -148,6 +143,7 @@ export default function ProjectItem({
           >
             {description}
           </p>
+
           <button
             onClick={() => setIsExpanded(!isExpanded)}
             className="text-blue-500 dark:text-blue-400 hover:text-blue-700 dark:hover:text-blue-300 text-xs mt-1 font-medium hover:cursor-pointer transition-all duration-300 ease-in-out w-fit focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 focus-visible:rounded-sm"
